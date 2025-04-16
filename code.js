05e6d17a1804271862c56f62374905fa79b430f1
noahvogt1 <96090888+noahvogt1@users.noreply.github.com>
2025-04-16 15:05:47 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,25 @@
 function tsp_hk(distance_matrix) {
-    return -1;
+    let minDist = Infinity;
+    let dist = 0;
+    for (let i = 0; i < distance_matrix.length; i++) {
+        dist = rec_hk(distance_matrix.map(row => [...row]), i);
+        if (dist < minDist) minDist = dist;
+    }
+    return minDist;
+}
+
+function rec_hk(distance_matrix, start) {
+    let minDist = Infinity;
+    let minNode = 0;
+    for (let i = 0; i < distance_matrix.length; i++) {
+        distance_matrix[i][start] = 0;
+        if (i != start && distance_matrix[start][i] != 0) {
+            if (distance_matrix[start][i] < minDist) {
+                minDist = distance_matrix[start][i];
+                minNode = i;
+            }
+        }
+    }
+    if (minDist === Infinity) return 0;
+    else return minDist + rec_hk(distance_matrix, minNode);
 }
