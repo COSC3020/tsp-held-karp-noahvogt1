22ab60e315627bf21b741a706555654ecd687bda
noahvogt1 <96090888+noahvogt1@users.noreply.github.com>
2025-04-21 14:42:36 -0600

    Update code.js

--- code.js
+++ code.js
@@ -3,14 +3,14 @@ function tsp_hk(distance_matrix) {
 
     for (let start = 0; start < distance_matrix.length; start++) {
         let visited = new Set([start]);
-        let dist = rec_hk(distance_matrix, start, visited, start);
+        let dist = rec_hk(distance_matrix, start, visited);
         if (dist < minDist) minDist = dist;
     }
     if (minDist == Infinity) return 0;
     return minDist;
 }
 
-function rec_hk(matrix, current, visited, memoization = {}, start) {
+function rec_hk(matrix, current, visited, memoization = {}) {
     let key = current + "|" + [...visited].sort();
 
     if (memoization[key] != undefined) {
@@ -26,7 +26,7 @@ function rec_hk(matrix, current, visited, memoization = {}, start) {
     for (let i = 0; i < matrix.length; i++) {
         if (!visited.has(i) && matrix[current][i] != 0) {
             visited.add(i);
-            let dist = matrix[current][i] + rec_hk(matrix, i, visited, memoization, start);
+            let dist = matrix[current][i] + rec_hk(matrix, i, visited, memoization);
             if (dist < minDist) minDist = dist;
             visited.delete(i);
         }
